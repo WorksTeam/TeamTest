ea19c75dca3eec0de50d037fa9b85972a349e93d
boyoutside <1535522850@qq.com>
2016-01-08 21:13:24 +0800

    测试GitHub项目

---
+++ src/main/java/org/fjzzy/TeamTest/App.java
@@ -0,0 +1,13 @@
+package org.fjzzy.TeamTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
